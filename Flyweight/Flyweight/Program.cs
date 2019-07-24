1d6c6178a432fbde8396d2e1b846323c27585730
Thiago Coelho <thiag0coelho@outlook.com>
2019-07-24 12:39:03 -0300

    Initial commit

---
+++ Flyweight/Flyweight/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Flyweight
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sentence = new Sentence("Hello World");
+            sentence[1].Capitalize = true;
+            var phrase = sentence.ToString();
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class Sentence
+    {
+        private List<WordToken> Phrase = new List<WordToken>();
+
+        public Sentence(string plainText)
+        {
+            foreach (var text in plainText.Split(" "))
+            {
+                Phrase.Add(new WordToken(text));
+            }
+        }
+
+        public WordToken this[int index]
+        {
+            get
+            {
+                return Phrase[index];
+            }
+        }
+
+        public override string ToString()
+        {
+            return String.Join(" ", Phrase);
+        }
+
+        public class WordToken
+        {
+            public bool Capitalize;
+
+            private string Word;
+
+            public WordToken(string word)
+            {
+                this.Word = word;
+            }
+
+            public override string ToString()
+            {
+                return Capitalize ? Word.ToUpper() : Word;
+            }
+        }
+    }
+}
---
+++ Strategy/Strategy/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Numerics;
+
+namespace Strategy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var test = new OrdinaryDiscriminantStrategy();
+            var test2 = new QuadraticEquationSolver(test);
+            test2.Solve(3, 2, -3);
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public interface IDiscriminantStrategy
+    {
+        double CalculateDiscriminant(double a, double b, double c);
+    }
+
+    public class OrdinaryDiscriminantStrategy : IDiscriminantStrategy
+    {
+        public double CalculateDiscriminant(double a, double b, double c)
+        {
+            return b * b - 4 * a * c;
+        }
+    }
+
+    public class RealDiscriminantStrategy : IDiscriminantStrategy
+    {
+        public double CalculateDiscriminant(double a, double b, double c)
+        {
+            double discriminant = b * b - 4 * a * c;
+
+            return discriminant >= 0 ? discriminant : double.NaN;
+        }
+    }
+
+    public class QuadraticEquationSolver
+    {
+        private readonly IDiscriminantStrategy strategy;
+
+        public QuadraticEquationSolver(IDiscriminantStrategy strategy)
+        {
+            this.strategy = strategy;
+        }
+
+        public Tuple<Complex, Complex> Solve(double a, double b, double c)
+        {
+            var discriminant = new Complex(strategy.CalculateDiscriminant(a, b, c), 0);
+            var rootDisc = Complex.Sqrt(discriminant);
+
+            var plus = (-b + rootDisc) / 2 * a;
+
+            var minus = (-b - rootDisc) / 2 * a;
+
+            return new Tuple<Complex, Complex>(plus, minus);
+        }
+    }
+}
